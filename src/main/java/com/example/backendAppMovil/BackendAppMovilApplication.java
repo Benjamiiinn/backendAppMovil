d2a65daf03aa1bdf32184b406b04bbf1e165293d
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2025-12-02 01:27:06 -0300

    Initial Commit

---
+++ src/main/java/com/example/backendAppMovil/BackendAppMovilApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendAppMovil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppMovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppMovilApplication.class, args);
+	}
+
+}

09217b489a9d36fd6e4af1f883dadda92bed5416
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2025-12-16 19:12:52 -0300

    Se actualiza el formulario de Compra

--- src/main/java/com/example/backendAppMovil/dto/CompraRequest.java
+++ src/main/java/com/example/backendAppMovil/dto/CompraRequest.java
@@ -6,15 +6,18 @@ import lombok.Data;
 
 @Data
 public class CompraRequest {
-    
+
+    private int idUsuario;
+
     private String metodoPago;
     
     private List<ItemCompra> items;
     
     @Data
     public static class ItemCompra {
-        private Integer rawgId;
+        private Integer idProducto;
         private Integer cantidad;
+        private Integer precio;
     }
 
 }
